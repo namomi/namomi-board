7b86b137210589a9d6d2ce3a0184b4b7086eaf40
namomi <xicodey@gmail.com>
2025-08-27 09:36:37 +0900

    [init] init

---
+++ service/article/src/main/java/namomi/board/article/service/PageLimitCalculator.java
@@ -0,0 +1,12 @@
+package namomi.board.article.service;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = lombok.AccessLevel.PRIVATE)
+public class PageLimitCalculator {
+
+    public static Long calculatePageLimit(Long page, Long pageSize, Long movablePageCount) {
+        return (((page - 1) / movablePageCount) + 1) * pageSize * movablePageCount + 1;
+    }
+}
+
