776f208e747e89020fbc554f82cbff4488221e64
namomi <xicodey@gmail.com>
2025-09-04 15:40:22 +0900

    [feat] いいね　設計実装

---
+++ service/like/src/main/java/namomi/board/like/repository/ArticleLikeRepository.java
@@ -0,0 +1,10 @@
+package namomi.board.like.repository;
+
+import namomi.board.like.entity.ArticleLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ArticleLikeRepository extends JpaRepository<ArticleLike, Long> {
+    Optional<ArticleLike> findByArticleIdAndUserId(Long articleId, Long userId);
+}
---
+++ service/like/src/test/java/namomi/board/like/api/LikeApiTest.java
@@ -0,0 +1,51 @@
+package namomi.board.like.api;
+
+import lombok.extern.slf4j.Slf4j;
+import namomi.board.like.service.response.ArticleLikeResponse;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+@Slf4j
+public class LikeApiTest {
+
+    RestClient restClient = RestClient.create("http://localhost:9002");
+
+    @Test
+    void likeAndUnlikeTest() {
+        Long articleId = 9999L;
+
+        like(articleId, 1L);
+        like(articleId, 2L);
+        like(articleId, 3L);
+
+        ArticleLikeResponse response1 = read(articleId, 1L);
+        ArticleLikeResponse response2 = read(articleId, 2L);
+        ArticleLikeResponse response3 = read(articleId, 3L);
+        log.info("response1 = {}", response1);
+        log.info("response2 = {}", response2);
+        log.info("response3 = {}", response3);
+
+        unlike(articleId, 1L);
+        unlike(articleId, 2L);
+        unlike(articleId, 3L);
+    }
+
+    void like(Long articleId, Long userId) {
+        restClient.post()
+                .uri("/v1/article-likes/articles/{articleId}/users/{userId}", articleId, userId)
+                .retrieve();
+    }
+
+    void unlike(Long articleId, Long userId) {
+        restClient.delete()
+                .uri("/v1/article-likes/articles/{articleId}/users/{userId}", articleId, userId)
+                .retrieve();
+    }
+
+    ArticleLikeResponse read(Long articleId, Long userId) {
+        return  restClient.get()
+                .uri("/v1/article-likes/articles/{articleId}/users/{userId}", articleId, userId)
+                .retrieve()
+                .body(ArticleLikeResponse.class);
+    }
+}
