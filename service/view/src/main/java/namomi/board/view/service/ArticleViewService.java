9e36a830fb3a068940a4089331fb8d9c7e92d4d8
namomi <xicodey@gmail.com>
2025-09-19 11:28:58 +0900

    [feat]  閲覧数アビューズ防止ポリシーの実装

--- service/view/src/main/java/namomi/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/namomi/board/view/service/ArticleViewService.java
@@ -1,21 +1,29 @@
 package namomi.board.view.service;
 
 import lombok.RequiredArgsConstructor;
-import namomi.board.view.repository.ArticleViewCountBackUpRepository;
 import namomi.board.view.repository.ArticleViewCountRepository;
+import namomi.board.view.repository.ArticleViewDistributedLockRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
-    private final ArticleViewCountBackUpRepository articleViewCountBackUpRepository;
-    private static final int BACK_UP_BACH_SIZE = 100;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
+    private static final int BACK_UP_BACH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     @Transactional
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BACH_SIZE == 0) {
             articleViewCountBackUpProcessor.backUp(articleId, count);
